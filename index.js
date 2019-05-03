4fb7d2e1f3359d74e4fe4cb6f5b573d89290cc1e
@JSDunn105 <j.s.dunn@icloud.com>
2019-05-02 21:00:48 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+  // let divide = function (a,b) {
+  //   return a/b;
+  // }
+let divide = () => 2000/100
+
+const square = side => side ** 2
+
+const add = (num1, num2) => {
+  return num1+num2
+}
+
